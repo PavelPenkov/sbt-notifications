6d4619a8bbf9936a598d701e490d60ca8b7540c9
Alexey Alekhin <laughedelic@gmail.com>
2017-10-22 04:22:34 +0200

    Proof-of-a-concept implementation of notification on any task completion

--- src/main/scala/me/penkov/sbt/NotificationsPlugin.scala
+++ src/main/scala/me/penkov/sbt/NotificationsPlugin.scala
@@ -5,11 +5,45 @@ import Keys._
 import complete.DefaultParsers._
 
 object NotificationsPlugin extends AutoPlugin {
+
+  // TODO: move it somewhere
+  def notifyOnDef[T](t: TaskKey[T]): Def.Initialize[Task[T]] = {
+    // TODO: some shortcut for getting default notifier
+    val notifier = Seq(
+      ToastNotifier,
+      LibNotifier,
+      MacNotifier
+    ).find(_.isAvailable) getOrElse NullNotifier
+
+    t.result.map {
+      case Inc(incomplete) => {
+        println(incomplete.toString)
+        notifier.notify(
+          s"⚠️ ${t.key.label}",
+          // TODO: try to show failure cause
+          "Failure!"
+        )
+        throw incomplete
+      }
+      case Value(value) => {
+        println(t.scope.toString)
+        notifier.notify(
+          s"✅ ${t.key.label}",
+          value.toString
+        )
+        value
+      }
+    }
+  }
+
   object autoImport {
   	val notifyFailureOnly = settingKey[Boolean]("Send notifications only if tests failed")
-   }
 
-  import autoImport._ 
+    def notifyOn[T](t: TaskKey[T]): Setting[Task[T]] = {
+      t := notifyOnDef(t).value
+    }
+  }
+  import autoImport._
 
   override lazy val projectSettings = Seq(
     testListeners += new NotifierTestListener(name.value, notifyFailureOnly.value),
