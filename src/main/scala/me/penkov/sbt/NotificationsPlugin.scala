9588b47237537bbc48906dbedd362ea37d06462c
Alexey Alekhin <laughedelic@gmail.com>
2017-10-22 05:17:15 +0200

    Added scoped key formatting and configurable success/failure formatters

--- src/main/scala/me/penkov/sbt/NotificationsPlugin.scala
+++ src/main/scala/me/penkov/sbt/NotificationsPlugin.scala
@@ -6,41 +6,81 @@ import complete.DefaultParsers._
 
 object NotificationsPlugin extends AutoPlugin {
 
-  // TODO: move it somewhere
-  def notifyOnDef[T](t: TaskKey[T]): Def.Initialize[Task[T]] = {
-    // TODO: some shortcut for getting default notifier
-    val notifier = Seq(
+  lazy val defaultNotifier: Notifier = {
+    Seq(
       ToastNotifier,
       LibNotifier,
       MacNotifier
-    ).find(_.isAvailable) getOrElse NullNotifier
+    )
+    .find(_.isAvailable)
+    .getOrElse(NullNotifier)
+  }
 
-    t.result.map {
-      case Inc(incomplete) => {
-        println(incomplete.toString)
-        notifier.notify(
-          s"⚠️ ${t.key.label}",
-          incomplete.causes.flatMap(_.directCause).mkString("• ", "\n• ", "")
-        )
-        throw incomplete
-      }
-      case Value(value) => {
-        println(t.scope.toString)
-        notifier.notify(
-          s"✅ ${t.key.label}",
-          value.toString
-        )
-        value
-      }
+  val defaultScopeMask: ScopeMask = new ScopeMask(
+    project = false,
+    config = true,
+    task = false,
+    extra = false
+  )
+
+  /** Formats [[TaskKey]] and its [[Scope]] with the given [[ScopeMask]] */
+  def formatScopedKey(
+    taskKey: TaskKey[_],
+    scopeMask: ScopeMask
+  ): String =
+    Scope.displayMasked(
+      taskKey.scope,
+      taskKey.key.label,
+      scopeMask
+    )
+
+  def defaultSuccessFormatter(taskKey: TaskKey[_]): String =
+    s"✅ ${formatScopedKey(taskKey, defaultScopeMask)}"
+  def defaultFailureFormatter(taskKey: TaskKey[_]): String =
+    s"⚠️ ${formatScopedKey(taskKey, defaultScopeMask)}"
+
+  // TODO: move it somewhere
+  /** Runs the task and depending on result shows success or failure notification */
+  def taskWithNotification[T](
+    taskKey: TaskKey[T],
+    successFormatter: TaskKey[_] => String,
+    failureFormatter: TaskKey[_] => String
+  ): Def.Initialize[Task[T]] = taskKey.result.map {
+    // Success
+    case Value(value) => {
+      defaultNotifier.notify(
+        successFormatter(taskKey),
+        value.toString
+      )
+      value
+    }
+    // Failure
+    case Inc(incomplete) => {
+      defaultNotifier.notify(
+        failureFormatter(taskKey),
+        incomplete.causes
+          .flatMap(_.directCause)
+          .mkString("• ", "\n• ", "")
+      )
+      throw incomplete
     }
   }
 
   object autoImport {
-  	val notifyFailureOnly = settingKey[Boolean]("Send notifications only if tests failed")
+    val notifyFailureOnly = settingKey[Boolean]("Send notifications only if tests failed")
 
-    def notifyOn[T](t: TaskKey[T]): Setting[Task[T]] = {
-      t := notifyOnDef(t).value
+    /** Wraps given task into a task with notification */
+    def notifyOn[T](
+      taskKey: TaskKey[T],
+      successFormatter: TaskKey[_] => String,
+      failureFormatter: TaskKey[_] => String
+    ): Setting[Task[T]] = {
+      taskKey := taskWithNotification(taskKey, successFormatter, failureFormatter).value
     }
+
+    /** Same `notifyOn` but with default formatters */
+    def notifyOn[T](taskKey: TaskKey[T]): Setting[Task[T]] =
+      notifyOn(taskKey, defaultSuccessFormatter, defaultFailureFormatter)
   }
   import autoImport._
 
